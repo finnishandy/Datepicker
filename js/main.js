b0db805ee9cae134b1db65126150203ee1975fc0
Sakari Ruoho <finnishandy@gmail.com>
2015-09-18 17:35:24 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/**
+ * Created by sakariruoho on 9/17/15.
+ */
+$('#datepickah').datepicker({startView: 'decade', format: 'dd/DD/YYYY'})
+    .on('changeDate', function (ev) {
+        console.log("date changed" + pad(ev.date.getDate()));
+        document.getElementById('date').setAttribute('value', pad(ev.date.getDate()));
+    })
+    .on('changeYear', function (ev) {
+        console.log("year changed: " + ev.date.getFullYear());
+        document.getElementById('year').setAttribute('value', ev.date.getFullYear());
+    })
+    .on('changeMonth', function (ev) {
+        console.log("month changed: " + pad(ev.date.getMonth()));
+        document.getElementById('month').setAttribute('value', pad(ev.date.getMonth()));
+    }
+);
+
+function pad(number) {
+    return (number < 10) ? '0' + number : number;
+}
